96a9ecd4ea3d5e87deeedbd0a92bd362a03b92ea
Ayeshajaaved <ayesha.javed@retailo.co>
2021-11-11 10:21:06 +0500

    route not found changes

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-const port = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
 const router = require("./app/routes");
 
 require("./app/configs/config");
+app.use("/", router);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/", router);
-app.use(function (req, res) {
-  res.status(404).send({ url: req.originalUrl + " not found" });
-});
 
+const port = process.env.PORT || 3000;
 app.listen(port);
 console.log("Server started on: " + port);
--- app/routes/index.js
+++ app/routes/index.js
@@ -6,4 +6,8 @@ const loginRouter = require("./login/index");
 router.use("/tasks", taskRouter);
 router.use("/api/login", loginRouter);
 
+router.use((req, res) => {
+  res.status(404).send({ url: req.originalUrl + " not found" });
+});
+
 module.exports = router;
